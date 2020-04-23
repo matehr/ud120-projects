b4c35eee98fc689574b5323de1ce73c02637352f
Matthew Ehrstein <skyhawkpilot05@yahoo.com>
2020-04-23 13:12:57 -0500

    fixed base code, nb and svm projects complete

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -23,8 +23,24 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 #########################################################
-### your code goes here ###
+from sklearn.svm import SVC
+import numpy as np
 
+model = SVC(kernel='rbf', C=10000)
+t0 = time()
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
+model.fit(features_train, labels_train)
+print ("training time:", round(time()-t0, 3), "s")
+
+t0 = time()
+print model.score(features_test, labels_test)
+print ("testing time:", round(time()-t0, 3), "s")
+
+predictions = model.predict(features_test)
+print predictions[10]
+print predictions[26]
+print predictions[50]
+
+print np.count_nonzero(predictions == 1)
 #########################################################
-
-
